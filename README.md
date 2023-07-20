bb4d548564cc9af7e21225fc8d80e2c0f61c4cea
Matthias Droth <matthias.droth@gmail.com>
2023-07-21 01:36:46 +0200

    course proceeds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WeaviateBasics
+
+Workshop video [_Introduction to Weaviate_](https://drive.google.com/file/d/1d2P1gBJzOJDL9CzgU91oLdaM6dnrCbLw/view?usp=drive_link)
---
+++ utils.py
@@ -0,0 +1,64 @@
+import torch
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+
+class EmbeddingGenerator:
+    def __init__(self, checkpoint):
+        # initialize the tokenizer and model for HuggingFace
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        self.model = AutoModel.from_pretrained(checkpoint)
+
+    def get_embedding(self, text):
+        # get text embeddings from HuggingFace model
+        inputs = self.tokenizer(text, return_tensors="pt")
+        with torch.no_grad():
+            model_output = self.model(**inputs)[0]
+        # taking the first element of model_output as embeddings
+        return model_output[0].numpy()
+
+    def get_embeddings(self, text_list):
+        # turn a list of texts into a list of embeddings
+        embeddings = []
+        for text in text_list:
+            embedding = self.get_embedding(text)
+            embeddings.append(embedding)
+        return embeddings
+    
+    def get_mean_embedding(self, text):
+        return np.mean(self.get_embedding(text), axis=0)
+
+    def get_mean_embeddings(self, text_list):
+        # turn a list of texts into a list of embeddings
+        embeddings = []
+        for text in text_list:
+            embedding = self.get_mean_embedding(text)
+            embeddings.append(embedding)
+        return embeddings
+
+class WeaviateOps:
+    def __init__(self, client_url, client_api_key, schema):
+        self.client=weaviate.Client(
+            url=client_url,
+            auth_client_secret=weaviate.AuthApiKey(api_key=client_api_key)
+        )
+        self.schema=schema
+
+    def create_one(self, object_id, class_name, text, embedding):
+        self.client.data_object.create({
+            "id": f"{object_id}",
+            "class": f"{class_name}",
+            "properties": {"text": text, "vector": embedding}
+        })
+        #
+        # assuming 'text' is your text and 'embedding' is its corresponding embedding
+object_data = {
+    "id": "your_object_id",  # replace with your object id
+    "class": "YourClassName",  # replace with your class name
+    "properties": {
+        "text": text,
+        "vector": embedding.tolist()  # convert the embedding to a list
+    }
+}
+
+# store the object in Weaviate
+client.data_object.create(object_data)
